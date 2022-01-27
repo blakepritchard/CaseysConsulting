f1e90f73abeed9be9281b2734d4af21af6fbef44
blakepritchard <blakepritchard@yahoo.com>
2022-01-26 21:51:43 -0600

    trying to render iframe

--- api/message/index.js
+++ api/message/index.js
@@ -1,9 +1,9 @@
 
-const constMsg = process.env.message;
+const constSecret = process.env.BotSecret;
 module.exports = async function (context, req) {
-    var strMessage = "Howdy"; // process.env.message;
+    var iframeChat = "<iframe src='https://webchat.botframework.com/embed/CaseysConsultingBot?s=" + constSecret +"'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>"; // process.env.message;
 
     context.res.json({
-        text: constMsg // process.env.message;
+        text: iframeChat 
     });
 };
