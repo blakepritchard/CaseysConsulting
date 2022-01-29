17b3f98f3024d4fe96217651eab3131b63d83833
Blake Pritchard <blakepritchard@yahoo.com>
2022-01-29 17:08:57 -0600

    still trying

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,7 @@
 
 
 module.exports = async function (context, req) {
-    const constSecret = process.env.message;
+    const constSecret = process.env.BotSecret;
     var iframeChat = "<iframe src='https://webchat.botframework.com/embed/CaseysConsultingBot?s=" + constSecret +"'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>"; // process.env.message;
 
     context.res.json({
